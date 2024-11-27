792c96c3241198c5ae6f89d64f36eaf11f047b1e
shortboy7 <thdwldnd7@naver.com>
2024-11-27 23:03:11 +0900

    fix: conversation-continue 제거

--- backend/app/services/conversation_service.py
+++ backend/app/services/conversation_service.py
@@ -9,13 +9,13 @@ from services.document_service import query_ensemble, clear_user_cache, save_chu
 from services.session_service import get_history, set_history, clear_history
 from utils.openai_model_manager import OpenAIModelManager
 from utils.utils import split_text
-from utils.prompts import prompt_morning, prompt_question, prompt_interest, prompt_night, prompt2, prompt3, prompt4, prompt5
+from utils.prompts import prompt_morning, prompt_question, prompt_interest, prompt_night, prompt2, prompt3, prompt4, \
+    prompt5
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.schema import Document
 
 from typing import List, Dict
-
 
 stored_summaries: Dict[str, List[str]] = {}
 
@@ -38,8 +38,8 @@ async def process_first_conversation(user_input):
     clear_history(user_id)
     init_file(user_info['age'], user_info['location'])
 
-    # current_hour = datetime.now().hour
-    current_hour = 20
+    current_hour = datetime.datetime.now().hour
+    # current_hour = 20
 
     if 7 <= current_hour <= 10:
         response = await execute_conversation(prompt_morning, model, user_info, weather_info, user_id)
@@ -54,10 +54,10 @@ async def process_first_conversation(user_input):
         response = await handle_question_case(user_info, model, user_id, question)
         response_json = json.loads(response.content)
         return {"message": response_json.get('message'), "question_id": question["question_id"]}
-    
+
     if 19 < current_hour <= 22:
         response = await execute_conversation(prompt_night, model, user_info, weather_info, user_id)
-    
+
     response_json = json.loads(response.content)
     return {"message": response_json.get('message')}
 
@@ -94,7 +94,7 @@ async def handle_no_question_case(user_info, model, user_id):
         print(context.page_content)
 
     response = await execute_conversation(
-        prompt_interest, model, user_info, None, user_id, 
+        prompt_interest, model, user_info, None, user_id,
         interest=interest, context=context_text
     )
     return response, interest['interest_id']
@@ -123,6 +123,9 @@ async def process_second_conversation(answer_input, background_tasks):
         ]
     )
 
+    formatted_time = datetime.datetime.now().strftime('%Y년 %m월 %d일 %p %I시 %M분')
+    formatted_time = formatted_time.replace('AM', '오전').replace('PM', '오후')
+
     runnable2 = prompt | model
     with_message_history = RunnableWithMessageHistory(
         runnable2,
@@ -130,7 +133,7 @@ async def process_second_conversation(answer_input, background_tasks):
         input_messages_key="input",
         history_messages_key="history",
     )
-    
+
     path = f"{user_info['age']}{user_info['location']}"
     context_text = await query_ensemble(user_id, answer, f"./data/{path}.txt")
 
@@ -139,20 +142,23 @@ async def process_second_conversation(answer_input, background_tasks):
         print(context_text[i].page_content)
 
     response = with_message_history.invoke(
-        {"input": answer, "context": context_text},
+        {"input": answer, "context": context_text, "current_time": formatted_time},
         config={"configurable": {"session_id": user_id}},
     )
 
     response_json = json.loads(response.content)
     message = response_json.get('message')
     score = response_json.get('score')
-    continue_conversation = response_json.get('continue_conversation')
+    # reference = response_json.get('reference')
+    # ref_num = response_json.get('ref_num')
 
     print(message)
     print(score)
+    # print(reference)
+    # print(ref_num)
 
-    if int(score) == 0 or int(continue_conversation) == 0:
-        print(f"score: {score}, continue: {continue_conversation}")
+    if int(score) == 0:
+        print(f"score: {score}")
 
         history = get_history(user_id)
         if len(history.messages) >= 2:
@@ -160,8 +166,8 @@ async def process_second_conversation(answer_input, background_tasks):
         print(get_history(user_id))
 
         background_tasks.add_task(finalize_conversation, user_id)
-        return {"message": message, "score": score, "continue_conversation": continue_conversation}
-    
+        return {"message": message, "score": score}
+
     return {"message": message, "score": score}
 
 
@@ -169,7 +175,7 @@ async def finalize_conversation(user_id):
     session_id = user_id
     history_copy = copy.deepcopy(get_history(session_id))
     user_info = await fetch_user(user_id)
-    path = f"{user_info['age']}{user_info['location']}" 
+    path = f"{user_info['age']}{user_info['location']}"
     summary = await generate_summary(user_id, history_copy)
     await update_memory_module(summary, f"./data/{path}.txt", user_id)
     save_chunks_to_file(summary, f"./data/{path}.txt")
@@ -181,6 +187,7 @@ async def finalize_conversation(user_id):
     stored_summaries[user_id] = [chunk.page_content for chunk in summary]
 
     clear_user_cache(user_id)
+
 
 ## demo 위한 코드
 def get_summary(user_id: str):
@@ -197,7 +204,7 @@ async def generate_summary(user_id, history):
         ]
     )
 
-    messages = prompt.format_messages(input="", history=history.messages, 
+    messages = prompt.format_messages(input="", history=history.messages,
                                       current_time=datetime.datetime.now().strftime('%Y-%m-%d'))
     response = model.invoke(messages)
     print("\n요약문 : ")
@@ -230,24 +237,24 @@ async def extract_user_interests(user_id, history):
 
 async def update_memory_module(new_summaries, data_path, user_id):
     existing_summaries = []
-    
+
     for new_summary in new_summaries:
         with open(data_path, 'r', encoding='utf-8') as file:
             existing_summaries = file.readlines()
-        
+
         print(f"existing summaries : {existing_summaries}")
-    
+
         similar_summaries = await query_ensemble(user_id, new_summary.page_content, data_path, 5)
         unique_summaries = []
         seen_contents = set()
-        
+
         print('--------------')
         for result in similar_summaries:
             if result.page_content not in seen_contents:
                 unique_summaries.append(result.page_content)
                 seen_contents.add(result.page_content)
         print(f"unique_summaries : {unique_summaries}")
-        
+
         modifications = await decide_modifications(new_summary.page_content, unique_summaries, user_id)
         print(f"update 대상 요약문 : {new_summary.page_content}")
         existing_summaries = update_summaries(existing_summaries, modifications, data_path)
@@ -263,9 +270,9 @@ async def decide_modifications(new_summary, similar_summaries, user_id):
     )
 
     messages = prompt.format_messages(input="",
-                                    new_summary=new_summary,
-                                    existing_summary=similar_summaries)
-    
+                                      new_summary=new_summary,
+                                      existing_summary=similar_summaries)
+
     response = model.invoke(messages)
     print(response.content)
 
